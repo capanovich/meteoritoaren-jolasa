54dc2f4ab9d252b73835c84a285fb0d5a51f40ac
capanovich <101406185+capanovich@users.noreply.github.com>
2022-05-20 13:55:37 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    puntua.move(1)
+})
+input.onButtonPressed(Button.B, function () {
+    puntua.move(-1)
+})
+let meteorito: game.LedSprite = null
+let puntua: game.LedSprite = null
+puntua = game.createSprite(2, 4)
 basic.forever(function () {
-	
+    basic.pause(randint(1000, 1500))
+    meteorito = game.createSprite(randint(0, 4), 0)
+    for (let index = 0; index < 4; index++) {
+        basic.pause(200)
+        meteorito.change(LedSpriteProperty.Y, 1)
+    }
+    if (meteorito.isTouching(puntua)) {
+        game.gameOver()
+    } else {
+        meteorito.delete()
+        game.addScore(1)
+    }
+    basic.pause(200)
 })
